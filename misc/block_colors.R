1180cb3723f4c72b357fc51711c0f3865a49c93a
Florian <floriansense@gmail.com>
2015-06-30 12:07:47 +0200

    The first version
    The first commit.

---
+++ misc/block_colors.R
@@ -0,0 +1,3 @@
+# Set the order of the blocks and their corresponding colors
+blocks <- c('vocab1', 'vocab2', 'vocab3', 'flags', 'maps', 'biopsych')
+block.col <- c('#E59B22', '#E67D22', '#E66422', '#16a085', '#2980b9', '#8e44ad')
